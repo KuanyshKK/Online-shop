034a74c131098a526033c7206a016acb429492b7
Kuanysh Kalikhanov <mrkunyabai@gmail.com>
2021-07-15 14:44:22 +0600

    Рефакторинг продуктов и производителей

--- src/main/java/com/example/homework51/controller/ManufacturerController.java
+++ src/main/java/com/example/homework51/controller/ManufacturerController.java
@@ -1,8 +1,8 @@
 package com.example.homework51.controller;
 
 import com.example.homework51.model.Manufacturer;
-import com.example.homework51.model.Product;
 import com.example.homework51.repository.ManufacturerRepository;
+import com.example.homework51.service.ManufacturerService;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -20,12 +20,13 @@ import java.util.List;
 @Controller
 public class ManufacturerController {
 
-    private  final ManufacturerRepository manufacturerRepository;
-    private final MongoTemplate mongoTemplate;
+    private final ManufacturerRepository manufacturerRepository;
+    private final ManufacturerService service;
 
-    public ManufacturerController(ManufacturerRepository manufacturerRepository, MongoTemplate mongoTemplate) {
+
+    public ManufacturerController(ManufacturerRepository manufacturerRepository, ManufacturerService service) {
         this.manufacturerRepository = manufacturerRepository;
-        this.mongoTemplate = mongoTemplate;
+        this.service = service;
     }
 
     @GetMapping("/manufacturerList")
@@ -52,11 +53,7 @@ public class ManufacturerController {
     @GetMapping("/manufacturerList/{id}")
     public String getManufacturer(Model model,
                              @PathVariable String id){
-        Manufacturer manufacturer = manufacturerRepository.findById(id).orElse(null);
-        if(manufacturer == null)
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND,
-                    String.format("Производитель с id %s не найден",id));
+        Manufacturer manufacturer = service.getById(id);
         model.addAttribute("manufacturer", manufacturer);
         return "manufacturerInfo";
     }
@@ -64,25 +61,17 @@ public class ManufacturerController {
     @GetMapping("/manufacturerList/{id}/update")
     public String getUpdateManufacturer(Model model,
                                    @PathVariable String id){
-        Manufacturer manufacturer = manufacturerRepository.findById(id).orElse(null);
-        if(manufacturer == null)
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND,
-                    String.format("Производитель с id %s не найден",id));
+        Manufacturer manufacturer = service.getById(id);
         model.addAttribute("manufacturer", manufacturer);
         return "manufacturerUpdate";
     }
 
     @PostMapping("/manufacturerList/{id}/update")
-    public String updateProduct(@PathVariable String id,
+    public String updateManufacturer(@PathVariable String id,
                                 @RequestParam String name,
                                 @RequestParam int code,
                                 @RequestParam String email){
-        Manufacturer manufacturer = mongoTemplate.findOne(Query.query(Criteria.where("_id").is(id)), Manufacturer.class);
-        if(manufacturer == null)
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND,
-                    String.format("Производитель с id %s не найден",id));
+        Manufacturer manufacturer = service.getById(id);
         manufacturer.setName(name);
         manufacturer.setCode(code);
         manufacturer.setEmail(email);
@@ -99,11 +88,7 @@ public class ManufacturerController {
     @GetMapping("/manufacturerList/{id}/delete")
     public String deleteManufacturer(
             @PathVariable String id){
-        Manufacturer manufacturer = manufacturerRepository.findById(id).orElse(null);
-        if(manufacturer == null)
-            throw new ResponseStatusException(
-                    HttpStatus.NOT_FOUND,
-                    String.format("Производитель с id %s не найден",id));
+        Manufacturer manufacturer = service.getById(id);
         manufacturerRepository.delete(manufacturer);
         return "redirect:/manufacturerList";
     }
--- src/main/java/com/example/homework51/repository/ManufacturerRepository.java
+++ src/main/java/com/example/homework51/repository/ManufacturerRepository.java
@@ -4,4 +4,5 @@ import com.example.homework51.model.Manufacturer;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ManufacturerRepository extends MongoRepository<Manufacturer, String> {
+
 }
